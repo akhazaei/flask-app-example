3254944b828cd9ae338839b7e74c952442d4f00b
akhazaei <arash.khazaei@gmail.com>
2023-03-24 13:13:41 -0400

    Log logtail handler info

--- src/api.py
+++ src/api.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 bp = Blueprint("api", __name__)
 
 @bp.route("/endpoint", methods=["GET"])
 def get_endpoint():
+    logtail_handler = current_app.logger.handlers[0]
+
+    current_app.logger.info("Logtail handler info %s", logtail_handler.__dict__)
+    
     return '1 + 1 = 2'
